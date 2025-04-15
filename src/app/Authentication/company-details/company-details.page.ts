4250fd34401e836bc7366357a89ee3102e4dd52e
MyGitFirs <fire20231@gmail.com>
2025-04-15 10:02:16 +0800

    2nd commit tim

--- src/app/Authentication/company-details/company-details.page.ts
+++ src/app/Authentication/company-details/company-details.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonCard } from '@ionic/angular';
 import { IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonButton, IonList, IonListHeader, IonItem, IonFab, IonFabButton, IonHeader, IonTitle, IonCol, IonRow, IonInput, IonGrid,IonSelect, IonSelectOption } from "@ionic/angular/standalone";
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-company-details',
   templateUrl: './company-details.page.html',
   styleUrls: ['./company-details.page.scss'],
-  imports: [IonCard, IonGrid, IonInput, IonRow, IonCol, IonTitle, IonHeader, IonFabButton, IonFab, IonItem, IonListHeader, IonList, IonButton, IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonSelect,IonSelectOption] // ✅ Import required Ionic components
+  imports: [ IonGrid, IonInput, IonRow, IonCol, IonTitle, IonHeader, IonFabButton, IonFab, IonItem, IonListHeader, IonList, IonButton, IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonSelect,IonSelectOption] // ✅ Import required Ionic components
 
 })
 export class CompanyDetailsPage implements OnInit {
